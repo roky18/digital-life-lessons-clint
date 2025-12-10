ed28f172daca0b7047fa09b4d0995081062ecb5d
roky18 <rokymax66@gmail.com>
2025-12-10 15:08:06 +0600

    10th update

--- src/Pages/MyLessons.jsx
+++ src/Pages/MyLessons.jsx
@@ -2,14 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../Hooks/useAuth";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
-import { MdFavorite, MdOutlineDateRange } from "react-icons/md";
-import { SlLike } from "react-icons/sl";
+import {
+  MdEditSquare,
+  MdFavorite,
+  MdOutlineDateRange,
+  MdOutlineInfo,
+} from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { AiFillLike } from "react-icons/ai";
+import Swal from "sweetalert2";
 
 const MyLessons = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: lessons = [] } = useQuery({
+  const { data: lessons = [], refetch } = useQuery({
     queryKey: ["myLessons", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/lessons?email=${user.email}`);
@@ -17,7 +24,31 @@ const MyLessons = () => {
       return res.data;
     },
   });
-  console.log(lessons);
+
+  const handleLessonDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do You want to Delete this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/lessons/${id}`).then((res) => {
+          if (res.data.deletedCount) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your Lesson has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div className="w-11/12 mx-auto">
       <h3 className="text-primary text-center font-semibold my-6 mb-10 text-3xl">
@@ -33,7 +64,7 @@ const MyLessons = () => {
               <th>Visibility</th>
               <th>Access level</th>
               <th>Details </th>
-              <th> stats</th>
+              <th> Stats</th>
               <th>Actions</th>
             </tr>
           </thead>
@@ -45,21 +76,41 @@ const MyLessons = () => {
                 <td>{lesson.privacy}</td>
                 <td>{lesson.access}</td>
                 <td>
-                  <button className="btn btn-dash btn-info">Details </button>
+                  <button className="btn btn-dash btn-info">
+                    <MdOutlineInfo />{" "}
+                  </button>
                 </td>
                 <td>
-                  <div className="flex-col text-lime-700 flex items-center">
-                    <MdOutlineDateRange />{lesson.createdAt} ◾
-                    <SlLike /> ◾
-                    <MdFavorite /> ◾
+                  <div className="flex-col flex items-center">
+                    <span className="flex items-center">
+                      <MdOutlineDateRange />
+                      {new Date(lesson.createdAt).toLocaleDateString()}
+                    </span>
+                    <div className="flex flex-col md:flex-row">
+                      <span className="flex text-blue-500 items-center">
+                        <AiFillLike />
+                        -- 99 ◾
+                      </span>
+                      <span className="flex text-red-500 items-center">
+                        <MdFavorite />
+                        -- 77
+                      </span>
+                    </div>
                   </div>
                 </td>
 
                 <td>
                   <div className="flex flex-col md:flex-row gap-2">
-                    <button className="btn btn-dash btn-warning">Update </button>
-                  
-                  <button className="btn btn-dash btn-error">Delete </button>
+                    <button className="btn btn-dash btn-warning">
+                      <MdEditSquare />{" "}
+                    </button>
+
+                    <button
+                      onClick={() => handleLessonDelete(lesson._id)}
+                      className="btn btn-dash btn-error"
+                    >
+                      <RiDeleteBin6Line />{" "}
+                    </button>
                   </div>
                 </td>
               </tr>
